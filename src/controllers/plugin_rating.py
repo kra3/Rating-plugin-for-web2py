bd4470f95afdc5ba1387762305bd44fe05a0632d
Arun Rajeevan <the1.arun@gmail.com>
2011-03-04 18:54:13 +0530

    creating git repo

---
+++ src/controllers/plugin_rating.py
@@ -0,0 +1,53 @@
+﻿# -*- coding: utf-8 -*- 
+
+# Licensed under the LGPL license version 3 
+# (http://www.gnu.org/licenses/lgpl.html)
+# copy right : Arun.K.R <the1.arun@gmail.com>
+
+def rate():
+    N=5
+    pm = db.plugin_rating_master
+    pa = db.plugin_rating_aux
+    tablename = request.args(0)
+    record_id = request.args(1)
+    rating = abs(float(request.vars.rating or 0)) 
+    
+    try:
+        db[tablename] #if there's no such table. Salute.
+        if rating>N or rating<0: raise Exception #similar if rating is simulated.
+        if not db[tablename][record_id]: raise Exception #also if there's no specified record in table
+        if not auth.user_id: raise Exception #user has to login to rate
+    except:
+        return ''
+        
+    master = db(pm.tablename==tablename)(pm.record_id==record_id).select().first()    
+    
+    if master:
+        master_rating, master_counter = master.rating, master.counter
+    else:
+        master_rating, master_counter = 0, 0
+        master=pm.insert(tablename=tablename,record_id=record_id,rating=master_rating,counter=master_counter)        
+        
+    record = db(pa.master==master)(pa.created_by==auth.user_id).select().first()
+        
+    if rating:
+        if not record:
+           record = db.plugin_rating_aux.insert(master=master,rating=rating,created_by=auth.user_id)
+           master_rating = (master_rating*master_counter + rating)/(master_counter+1)
+           master_counter = master_counter + 1
+        else:
+           master_counter = master_counter
+           master_rating = (master_rating*master_counter - record.rating + rating)/master_counter
+           record.update_record(rating=rating)
+           
+        master.update_record(rating=master_rating, counter=master_counter)
+        
+    try:  
+        db[tablename][record_id]['rating']
+        #having a rating column in 'tablename' isn't mandatory
+    except:
+        return ''
+    else:
+        db[tablename][record_id].update_record(rating=master_rating)
+        
+    return ''
---
+++ src/models/plugin_rating.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*- 
+
+# Licensed under the LGPL license version 3 
+# (http://www.gnu.org/licenses/lgpl.html)
+# copy right : Arun.K.R <the1.arun@gmail.com>
+
+db.define_table('plugin_rating_master',
+   Field('tablename'),
+   Field('record_id','integer'),
+   Field('rating','double'),
+   Field('counter','integer'))
+   
+db.define_table('plugin_rating_aux',
+   Field('master',db.plugin_rating_master),
+   Field('rating','double'),
+   Field('created_by',db.auth_user))
+
+response.files.append(URL(r=request, c='static', f='plugin_rating/jquery.rating.css'))
+response.files.append(URL(r=request, c='static', f='plugin_rating/jquery.rating.js'))
+
+def plugin_rating(tablename,record_id, splitstars=False):
+    import uuid    
+    id = uuid.uuid4() 
+    row=db(db.plugin_rating_master.tablename==tablename)(db.plugin_rating_master.record_id==record_id).select().first()
+    rating = row.rating if row else 0
+    callback = URL('plugin_rating', 'rate', args = [tablename,record_id])    
+    star = 0.5 if splitstars else 1   
+    return TAG[''](DIV(_id='star'+str(id),_class='rating'),
+                   SCRIPT("jQuery(document).ready(function(){jQuery('%(uid)s').rating('%(callback)s',{increment:%(star)s, maxvalue:5, curvalue:%(rating)s});});" % dict(uid='#star'+str(id), callback=callback, star=star, rating=rating)))
